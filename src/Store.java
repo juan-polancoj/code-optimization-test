c1204e20cda7270d0e40ea0c69aaa38a0a28c2cc
jdpolanco <jdpolancoj@ufpso.edu.co>
2024-09-14 07:53:02 -0500

    refactor complete for quiz

--- src/Store.java
+++ src/Store.java
@@ -1,19 +1,27 @@
 public class Store {
     public static void main(String[] args) {
-        int p1 = 15;
-        int p2 = 10;
-        int p3 = 5;
+        //cambiar las ventas hechas como constantes y los nombres de sus variables 
+        final int SALES_DAY1 = 15;
+        final int SALES_DAY2 = 10;
+        final int SALES_DAY3 = 5;
+        //cambiar los numeros magicos por numeros de personas que compran
+        final int NUMBER_PEOPLE_DAY1 = 2;
+        final int NUMBER_PEOPLE_DAY2 = 3;
+        final int NUMBER_PEOPLE_DAY3 = 4;
+        // añadir 50 como una constante
+        final int averageSales = 50;
 
-        int total1 = p1 * 2;
-        int total2 = p2 * 3;
-        int total3 = p3 * 4;
+        //ser mas claro con las variables de totales de ventas 
+        int totalSales1 = SALES_DAY1 * NUMBER_PEOPLE_DAY1;
+        int totalSales2 = SALES_DAY2 * NUMBER_PEOPLE_DAY2;
+        int totalSales3 = SALES_DAY3 * NUMBER_PEOPLE_DAY3;
 
-        int totalSales = total1 + total2 + total3;
+        int totalSales = totalSales1 + totalSales2 + totalSales3;
 
-        if (totalSales > 50) {
-            System.out.println("Good sales performance");
+        if (totalSales > averageSales ) {
+            System.out.println("Good sales performance"); //Buen desempeño en ventas
         } else {
-            System.out.println("Low sales performance");
+            System.out.println("Low sales performance"); // Bajo rendimiento de ventas
         }
     }
 }
--- src/TaxCalculation.java
+++ src/TaxCalculation.java
@@ -2,11 +2,16 @@ public class TaxCalculation {
     public static void main(String[] args) {
         double productPrice1 = 100;
         double productPrice2 = 200;
-        double tax1 = productPrice1 * 0.15;
-        double tax2 = productPrice2 * 0.10;
+        // cambiar los valore magicos por constantes
+        final double TAX_RATE_THRESHOLD = 50;
+        final double TAX_RATE_HIGH = 0.15;
+        final double TAX_RATE_LOW = 0.10;
+        // sustituir los valores magicos por sus constantes y se multiplican 
+        double tax1 = productPrice1 * TAX_RATE_HIGH;
+        double tax2 = productPrice2 * TAX_RATE_LOW;
         double totalTax = tax1 + tax2;
         
-        if (totalTax > 50) {
+        if (totalTax > TAX_RATE_THRESHOLD) {
             System.out.println("High total tax: " + totalTax);
         } else {
             System.out.println("Low tax");
